35a44263383cd6b0003b6bb764f12cfd4d6a5867
gnouc1999 <cuongrice1999@gmail.com>
2019-10-09 10:00:49 +0700

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/gamedochuapplication/Level1Activity.java
    #	app/src/main/java/com/example/gamedochuapplication/LevelActivity.java
    #	app/src/main/java/com/example/gamedochuapplication/LoginActivity.java
    #	app/src/main/java/com/example/gamedochuapplication/SignUpActivity.java
    #	app/src/main/java/com/example/gamedochuapplication/VocabularyTopicFavoriteActivity.java
    #	app/src/main/res/drawable/bnt_login.xml
    #	app/src/main/res/drawable/bnt_sign_up.xml
    #	app/src/main/res/drawable/btn_bg.xml
    #	app/src/main/res/drawable/btn_cancel.xml
    #	app/src/main/res/drawable/custom_tv.xml
    #	app/src/main/res/layout/activity_intro.xml
    #	app/src/main/res/layout/activity_level.xml
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_start.xml
    #	app/src/main/res/layout/layout_endgame.xml
    #	app/src/main/res/layout/layout_list_word.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/gamedochuapplication/SignUpActivity.java
+++ app/src/main/java/com/example/gamedochuapplication/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.gamedochuapplication;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -9,17 +8,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-
-//import com.android.volley.Request;
-//import com.android.volley.RequestQueue;
-//import com.android.volley.Response;
-//import com.android.volley.toolbox.StringRequest;
-//import com.example.gamedochuapplication.user_data.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,28 +18,14 @@ public class SignUpActivity extends AppCompatActivity {
 
     EditText edtFullname, edtAge, edtDoB, edtEmail, edtUsername, edtPassword;
     Button btnSignUp;
-    ImageButton btnBackSignup;
-    Intent intent;
     ProgressDialog progressDialog;
     private static final String TAG = "SignUpActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //hide actionbar
-        ActionBar actionBar = getSupportActionBar();
-
-        actionBar.hide();
-        //full screen
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        setContentView(R.layout.layout_sign_up);
-
-
+        setContentView(R.layout.layout_sign_up);
         init();
-
     }
-
 
     private void init() {
         btnSignUp = findViewById(R.id.btn_signUp);
@@ -59,7 +35,6 @@ public class SignUpActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edt_sign_up_email);
         edtUsername = findViewById(R.id.edt_sign_up_username);
         edtPassword = findViewById(R.id.edt_sign_up_password);
-        btnBackSignup = findViewById(R.id.btnBackSignup);
     }
 
     void clickSignUp(){
@@ -67,16 +42,6 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-            }
-        });
-    }
-
-    void clickBackSignup(){
-        btnBackSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
             }
         });
     }
