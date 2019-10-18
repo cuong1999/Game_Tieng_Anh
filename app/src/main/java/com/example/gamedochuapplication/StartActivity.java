62b348c788026df7c5ec76de077bdbcc06a47810
thanhphu247 <52515997+thanhphu247@users.noreply.github.com>
2019-10-18 08:24:46 +0700

    firebase

--- app/src/main/java/com/example/gamedochuapplication/StartActivity.java
+++ app/src/main/java/com/example/gamedochuapplication/StartActivity.java
@@ -1,22 +1,34 @@
 package com.example.gamedochuapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class StartActivity extends AppCompatActivity {
     //Controls
     ImageButton imvBack, imvFavorite, imvHome, imvRestore, imvNext;
     ListView lv_project;
+    ArrayAdapter<String> adapter;
+    String TAG = "FIREBASE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +41,7 @@ public class StartActivity extends AppCompatActivity {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
+
         init();
     }
 
@@ -39,6 +52,28 @@ public class StartActivity extends AppCompatActivity {
         imvHome = findViewById(R.id.imv_home);
         imvRestore = findViewById(R.id.imv_restore);
         imvNext = findViewById(R.id.imv_next);
+
+        adapter = new ArrayAdapter<>(this, R.layout.item_lv_project);
+        lv_project.setAdapter(adapter);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("24/data");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                adapter.clear();
+                for (DataSnapshot data: dataSnapshot.getChildren()){
+                    //String key = data.getKey();
+                    String value = data.getValue().toString();
+                    adapter.add(value);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
 
         imvBack.setOnClickListener(new View.OnClickListener() {
             @Override
